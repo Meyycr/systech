ad8137ae490713713917cf6fc677ad89bcd50ec6
Meyycr <meyycr@gmail.com>
2023-10-19 20:48:26 +0200

    def

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -9,10 +9,7 @@ const CardDeal = () => {
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/>in few easy steps.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Arcu tortor,
-          purus in mattis at sed integer faucibus.
-          Aliquet quis aliquet eget mauris tortor.ç 
-          Aliquet ultrices ac, ametau.
+          Here you can write your text !
         </p>
         <Button styles='mt-10'/>
       </div>
